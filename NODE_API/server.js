3933c4b82ec95fdc853ae5a3cdbbd25673790a06
Welington Oliveira <56723945+kgton12@users.noreply.github.com>
2020-01-04 11:38:24 -0300

    Add files via upload

---
+++ NODE_API/server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const requireDir = require('require-dir');
+
+// Iniciando o App
+const app = express();
+app.use(express.json());
+
+// Iniciando o DB
+mongoose.connect('mongodb://localhost:27017/nodeapi', {
+  useNewUrlParser: true
+});
+requireDir('./src/models'); //requireDir é uma pacote que auxilia a funcao require
+
+
+
+// Rotas
+app.use('/api',require('./src/routes'));
+
+app.listen(3001);
